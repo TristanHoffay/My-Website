9b7389fb481c7151b42a5c9d4f5eb9bfa4828680
TristanHoffay <38638607+TristanHoffay@users.noreply.github.com>
2024-07-31 16:47:37 -0700

    Added cool effect to coding projects page
    Added scrolling green text to the coding projects page cause it looks cool. Also added dummy project entry for website.

---
+++ codebars.js
@@ -0,0 +1,99 @@
+speed = 20;
+frequency = 30;
+trailFade= 2;
+barcount = 0;
+// If shots should generate at random space instead of the top
+midcolShots = false;
+function Bar(htmlElement) {
+    this.id = barcount;
+    barcount++;
+    this.element = htmlElement;
+    this.height = htmlElement.offsetHeight;
+    this.width = htmlElement.offsetWidth;
+    this.fontSize = parseInt(window.getComputedStyle(htmlElement).fontSize);
+    this.columns = Math.floor(this.width / this.fontSize*1.67);
+    this.rows = Math.floor(this.height / this.fontSize*0.84);
+    this.charCount = this.columns * this.rows;
+    // Array of 'shots' down the text. Each has x(col index) and y(row index)
+    this.shots = [];
+    this.cooldown = frequency;
+    console.log(this.columns, this.rows, this.charCount);
+    this.updateColor = function(charSpan, transition, transparency) {
+        charSpan.style.transition = `color ${transition}s`;
+        charSpan.style.color = `rgba(0, 255, 0, ${transparency})`;
+    };
+    // Assigns new char to given <span> element (for individual chars)
+    this.newChar = function (charSpan) {
+        newchar = Math.floor(Math.random() * 94 + 33);
+        charSpan.innerHTML = String.fromCharCode(newchar);
+    };
+    this.getChar = function(col, row) {
+        if (col < this.columns && row < this.rows) {
+            char = document.getElementById(`char[${col},${row}]${this.id}`)
+            return char;
+        }
+        else return null;
+    };
+    this.updateShots = function() {
+        for (let i = 0; i < this.shots.length; i++) {
+            shot = this.shots[i];
+            // Set previous char
+            charSpan = this.getChar(shot[0], shot[1]-1);
+            if (charSpan) {
+                this.updateColor(charSpan, trailFade, 0);
+            }
+            // Set char at shot
+            charSpan = this.getChar(shot[0], shot[1]);
+            if (charSpan) {
+                this.updateColor(charSpan, 0, 1);
+                this.newChar(charSpan);
+                this.shots[i] = [shot[0], shot[1]+1];
+            }
+            else {
+                this.shots.splice(i,1);
+            }
+        }
+    };
+    this.update = function () {
+        // New shot at random column
+        this.cooldown -= frequency;
+        if (this.cooldown < 0) {
+            // New shot
+            col = Math.floor(Math.random() * this.columns)
+            row = midcolShots ? Math.floor(Math.random() * this.rows) : 0
+            this.shots.push([col, row])
+            // Reset cooldown
+            this.cooldown = Math.floor(Math.random() * 50) + 70;
+        }
+        this.updateShots();
+    };
+    // Add spans of chars to bar
+    this.element.innerHTML = "";
+    for (let r = 0; r < this.rows; r++){
+        for (let c = 0; c < this.columns; c++) {
+            newChar = document.createElement("span");
+            this.newChar(newChar);
+            newChar.id = `char[${c},${r}]${this.id}`;
+            newChar.style.transition = `color ${0}s`;
+            this.element.appendChild(newChar);
+        }
+    }
+    console.log(this.width);
+    this.element.style.width = `${this.width}px`;
+}
+function initBars() {
+    bar_elements = document.getElementsByClassName("sidebar-code");
+    bars = []
+    for (let bar of bar_elements){
+        newbar = new Bar(bar);
+        bars.push(newbar);
+    }
+    return bars
+}
+bars = initBars();
+function updateBars() {
+    for (let bar of bars){
+        bar.update();
+    }
+}
+window.setInterval(updateBars, 1000/speed)
